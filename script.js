a56af3237c28fbe7a4ac401fb2286577a58f20ac
matt-davz <davies.matt.02@gmail.com>
2023-07-22 19:27:01 +0700

    added interactivity to settings inner buttons

--- script.js
+++ script.js
@@ -9,8 +9,33 @@ function openSettings(){
 }
 
 function openMixer(){
-    
+    settings.style.display = "none";
+    time.style.display = "none";
+    mixer.style.display = "block";
 }
+
+const timersBtn = document.getElementById('timers-button');
+const timersOptions = document.getElementById('timers-options');
+const soundsBtn = document.getElementById('sounds-button')
+const soundsOptions = document.getElementById('sounds-options');
+
+function openTimers(){
+    timersBtn.style.textDecoration = "underline";
+    soundsBtn.style.textDecoration = "none";
+    timersOptions.style.display = "flex";
+    soundsOptions.style.display= "none";
+}
+
+timersBtn.addEventListener('click',openTimers);
+
+function openSounds(){
+    timersBtn.style.textDecoration = "none";
+    soundsBtn.style.textDecoration = "underline";
+    timersOptions.style.display = "none";
+    soundsOptions.style.display= "flex";
+}
+
+soundsBtn.addEventListener('click',openSounds);
 
 function exit(){
     settings.style.display = "none";
@@ -19,8 +44,10 @@ function exit(){
 }
 const settingClick = document.getElementsByClassName('setting-contianer')[0];
 const exitClick = document.querySelectorAll('.exit-container');
+const mixerClick = document.getElementsByClassName('mixer')[0];
 
 exitClick.forEach(element => {
     element.addEventListener('click', exit);
 });
 settingClick.addEventListener('click',openSettings);
+mixerClick.addEventListener('click',openMixer)
